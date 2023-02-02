730f60196f99f8163bb99b8bfc3fea6ff1bd8235
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-02-02 21:56:45 +0300

    feat: create modal container

---
+++ assets/js/buttons.js
@@ -0,0 +1,10 @@
+function handleAddTaskClick() {
+  const iconAddTaskEl = document.getElementById("icon-add-task");
+}
+function bbb() {}
+
+function onDOMContentLoaded() {
+  bbb();
+}
+
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
